bc8533129eb382739d5971561074b1fb858525d9
AlphaKorsun <48954474+AlphaKorsun@users.noreply.github.com>
2020-03-04 13:41:35 +0100

    Create Generic Swap.cs

---
+++ 01_C#/Generic Swap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GenericSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 4;
+            int second = 5;
+            Console.WriteLine("First: "+first.ToString());
+            Console.WriteLine("Second: "+second.ToString());
+            Swap<int>(ref first, ref second);
+            Console.WriteLine("First: "+first.ToString());
+            Console.WriteLine("Second: "+second.ToString());
+        }
+
+        static void Swap<T>(ref T input1, ref T input2)
+        {
+            T temp = default(T);
+
+            temp = input2;
+            input2 = input1;
+            input1 = temp;
+        }
+    }
+}
